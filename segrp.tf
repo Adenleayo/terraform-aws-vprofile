f357a357d45232785cb643f88762ec62028bfb62
Adenleayo <boluwatifeadenle@gmail.com>
2024-11-25 14:38:53 +0100

    security group

---
+++ segrp.tf
@@ -0,0 +1,88 @@
+resources "aws_security_group" "vprofile-bean-elb-sg" {
+    name        = "vprofile-bean-elb-sg"
+    description = "security group for bean elb security group"
+    vpc_id      = module.vpc.vpc_id
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+}
+
+resources "aws_seurity_group" "vprofile-bastion-sg" {
+    name        = "vprofile-bastion-sg"
+    description = "security group for bastion host security group"
+    vpc_id      = module.vpc.vpc_id
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = [var.myip]
+    }
+
+}
+
+recources "aws_secrity_group" "vprofile-prod-sg" {
+    name        = "vprofile-prod-sg"
+    description = "security group for production environment"
+    vpc_id      = module.vpc.vpc_id
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = [aws_secrity_group.vprofile_bastion-sg.id]
+    }
+}
+
+resources "aws_secrity_group" "vprofile_backend_sg" {
+    name        = "vprofile-backend-sg"
+    description = "security group for backend"
+    vpc_id      = module.vpc.vpc_id
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = [aws_secrity_group.vprofiile-prod-sg.id]
+    }
+
+
+}
+
+resource "aws_security_group_rule" "sec_group_allow_itself" {
+    security_group_id = aws_security_group.vprofile_backend_sg.id
+    source_security_group_id = aws_security_group.vprofile_backend_sg.id
+    type              = "ingress"
+    from_port         = 0
+    to_port           = 65535
+    protocol          = "tcp"
+
+}
+
